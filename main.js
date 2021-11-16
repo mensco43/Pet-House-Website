fd2a828c7d61d5f1873d3256f6d03329fc673ab9
Menim Joseph <59745948+mensco43@users.noreply.github.com>
2021-11-16 15:30:53 +0100

    initialcommit

---
+++ main.js
@@ -0,0 +1,105 @@
+
+var dog =[
+    {
+        kind:"American Breed",
+        price:"$100"
+    },
+    {
+        kind:"Pomeranian",
+        price: "$90"
+    },
+    {
+        kind: "American Breed", 
+        price:"$200"
+    },
+    {
+        kind: "British Shorthair",
+        price:"$150"
+    },
+    {
+        kind: "Beagle",
+        price:"$200"
+    },
+    {
+        kind: "Pug",
+        price:"$250"
+    },
+    {
+        kind: "Pug",
+        price:"$300"
+    },
+    {
+        kind: "Pug",
+        price:"$400"
+    },
+    {
+        kind: "Pug",
+        price:"$200"
+    },
+    {
+        kind: "Pug",
+        price:"$400"
+    },
+    {
+        kind: "Pug",
+        price:"$100"
+    },
+    {
+        kind: "Pug",
+        price:"$350"
+    }
+]
+for (i=0; i< dog. length; i++){
+    if(dog[i].price < $400){
+        document.write(dog [i] .kind +"," )
+    }
+}
+//console.log (dog[0].kind)
+/*var dog={
+    kind:"American Breed",
+    price:"$100"
+}
+var dog={
+    kind:"Pomeranian",
+    price: "$90"
+}
+var dog={
+    kind: "American Breed", 
+    price:"$200"
+}
+var dog={
+    kind: "British Shorthair",
+    price:"$150"
+}
+var dog={
+    kind: "Beagle",
+    price:"$200"
+}
+var dog={
+    kind: "Pug",
+    price:"$250"
+}
+var dog={
+    kind: "Pug",
+    price:"$300"
+}
+var dog={
+    kind: "Pug",
+    price:"$400"
+}
+var dog={
+    kind: "Pug",
+    price:"$200"
+}
+var dog={
+    kind: "Pug",
+    price:"$400"
+}
+var dog={
+    kind: "Pug",
+    price:"$100"
+}
+var dog={
+    kind: "Pug",
+    price:"$350"
+}*/
